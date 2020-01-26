a4fcbdb5dec7b51d2427cc98eff52fe09d0e89f5
FlyingColors02 <shraduborse97@gmail.com>
2020-01-26 22:29:17 +0530

    #shraddha borse: logical operator

---
+++ main.js
@@ -0,0 +1,33 @@
+//logical operator
+
+/* && operator */
+numValue1=5;
+numValue2=20;
+if(numValue1<10 && numValue1!==0)//true
+{
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+if(numValue1>10 && numValue1!=0)//false
+{
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+//here both condition should be true.
+
+///////////////////////////////////////////////////////////////////
+
+/* || operator */
+if(numValue2===20 || numValue2>60)//true
+{
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+//here either one condition should be true.
